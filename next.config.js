a1d90cc4b32b9bda86d6cfb9b6f1b90373bb8537
viviendasvirtuo <viviendasvirtuo@gmail.com>
2026-06-10 13:42:30 +0200

    fix: añade remotePatterns para Unsplash en next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ['@supabase/supabase-js'],
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
